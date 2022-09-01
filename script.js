323c21f1b2dc10368c0461553f63336a40b6966f
Gabriel Gusso <gabriel.gusso@hotmail.com>
2022-09-01 17:00:17 -0300

    Acessando as peguntas/escolhendo as respostas e marcando as certar/erradas

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let arrayQuizzes = []
 pegarQuizzes()
-
+//SOLICITAR OS QUIZZES NA API
 function pegarQuizzes() {
 const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes")
 promessa.then(quizzes)
 }
-
+//RENDERIZAR OS QUIZZES NA TELA INICIAL
 function quizzes(resposta){
     arrayQuizzes = resposta.data
     console.log(arrayQuizzes)
@@ -19,7 +19,7 @@ function quizzes(resposta){
     }
     
 }
-
+//AO CLICAR NO QUIZZ LEVAR NA TELA DE PEGUNTAS
 function novaPag(element){
     const id = element.classList.value
     const promessa = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`)
@@ -30,13 +30,14 @@ function novaPag(element){
     // main2.classList.remove('display')
 }
 
+//TELA DE PERGUNTAS
 function quizzPergunta(resposta){
     const image = resposta.data.image
     const title = resposta.data.title
     const questions = resposta.data.questions
     const body = document.querySelector('body')
     body.innerHTML = body.innerHTML + ` <div class="main2">
-    <div class="top" ><h1>${title}</h1><img src="${image}"/> </div>`
+    <div class="top" ><div class="escuro"></div><h1>${title}</h1><img src="${image}"/> </div>`
 
     for(let i = 0; i < questions.length; i++){
         
@@ -50,13 +51,32 @@ function quizzPergunta(resposta){
       
       for(let j = 0; j < questions[i].answers.length; j++){
 
-        respostasCaixa[i].innerHTML = respostasCaixa[i].innerHTML +  `<div class="reposta-img"><img src="${questions[i].answers[j].image}" alt=""><h1>${questions[i].answers[j].text}</h1></div>
+        respostasCaixa[i].innerHTML = respostasCaixa[i].innerHTML +  `<div class="resposta-img ${questions[i].answers[j].isCorrectAnswer}" onclick="avaliarResposta(this)"><img src="${questions[i].answers[j].image}" alt=""><h1>${questions[i].answers[j].text}</h1></div>
       `
       }
     }
    body.innerHTML = body.innerHTML + `
    </div>`
 
+}
+
+//MARCAR RESPOSTA CERTA OU ERRADA AO CLICAR(TELA DE PERGUNTAS)
+function avaliarResposta(element){
+  if(element.classList[2] !== 'opacity'){
+  const allAswers = element.parentNode.children
+  
+  for(let i = 0; i < allAswers.length; i++){
+    allAswers[i].classList.add('opacity')
+
+    if(allAswers[i].classList[1] === "false"){
+      allAswers[i].classList.add('incorreto')
+    } else {
+      allAswers[i].classList.add('correto')
+    }
+  }
+
+  element.classList.remove('opacity')
+  }
 }
 
 
